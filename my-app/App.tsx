9dc66b9d177c756d40be7fdcd8ed3f2f0f391e10
bablu <aniketpanchal07@gmail.com>
2024-03-03 15:34:07 +0530

    native react app with native-wind and typescript

---
+++ my-app/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import {  Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View 
+    className=" bg-red-400 justify-center grid place-items-center"
+    // style={styles.container}
+    >
+      <Text>Open up App.tsx to start working on your app hellofsf!</Text>
+      {/* <StatusBar style="auto" /> */}
+    </View>
+
+ 
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
